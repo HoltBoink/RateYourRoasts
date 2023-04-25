3001d0fefdfdcde61111c23a107e4d503b9398af
HoltBoink <boinkh@bc.edu>
2023-04-25 16:22:15 -0400

    Initial Commit

---
+++ RateYourRoasts/RateYourRoastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateYourRoastsApp.swift
+//  RateYourRoasts
+//
+//  Created by Holt Boink on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RateYourRoastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
